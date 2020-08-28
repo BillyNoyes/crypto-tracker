659040ef21efb715573e3a9e93d82b48ca8af19a
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-28 17:04:58 +0100

    Built Coins component

--- src/Coins.js
+++ src/Coins.js
@@ -8,12 +8,16 @@ function Coins({ name, price, symbol, marketcap, volume, image, priceChange }) {
         <div className="coins__coin">
           <img src={image} alt={name} />
           <h1>{name}</h1>
-          <p className="coin__symbol">{symbol}</p>
+          <p className="coins__symbol">{symbol}</p>
         </div>
-        <div className="coin__data">
-          <p className="coin__price">£{price}</p>
-          <p className="coin__volume">£{volume.toLocaleString()}</p>
-          <p className={`coin__percent ${priceChange < 0 ? `red` : `green`}`}>
+        <div className="coins__data">
+          <p className="coins__price">£{price}</p>
+          <p className="coins__volume">£{volume.toLocaleString()}</p>
+          <p
+            className={`coins__percent ${
+              priceChange < 0 ? `coin__red` : `coin__green`
+            }`}
+          >
             {priceChange.toFixed(2)}%
           </p>
           <p className="coin-marketcap">
