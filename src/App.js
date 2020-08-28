186bb4c0a04d5ab307104ffdfc5bd82c64287619
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-28 16:08:47 +0100

    Pulling data from API using Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Coin from "./Coin";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="app">
-      <h1></h1>
+      <div className="app__search">
+        <h1 className="app__text">Search a currency</h1>
+        <form>
+          <input className="app__input" type="text" placeholder="search" />
+        </form>
+      </div>
+      <Coin />
     </div>
   );
 }
